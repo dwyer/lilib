703a974245f7e090514e60ae2e5a8d4cde46d4ce
Casey Dwyer <caseydwyer@gmail.com>
2012-11-21 01:45:08 -0800

    rel.scm tests

---
+++ tests/rel-test.scm
@@ -0,0 +1,38 @@
+(load "../rel.scm")
+
+(define (print obj)
+  (write obj)
+  (newline))
+
+(define (print-rel rel)
+  (for-each print rel))
+
+(define person
+  '((name age weight)
+    ("Harry" 34 80)
+    ("Sally" 28 64)
+    ("George" 29 70)
+    ("Helena" 54 54)
+    ("Peter" 34 80)))
+
+(define db
+  '((employee
+      ((name id dept)
+       ("harry" 1 "finance")
+       ("sally" 2 "sales")
+       ("george" 3 "finance")
+       ("harriet" 4 "sales")))
+    (dept
+      ((dept manager)
+       ("finance" "george")
+       ("sales" "harriet")
+       ("production" "charles")))))
+
+(print person)
+(print (project '(weight name) person))
+(print (select '(>= age 34) person))
+(print (select '(= age weight) person))
+
+(let ((employee (relation 'employee db))
+      (dept (relation 'dept db)))
+  (print-rel (join employee dept)))
